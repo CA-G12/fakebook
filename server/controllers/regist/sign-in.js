4c28ea26c2d9f0b42957e97fc447c2a4c7a3d45e
Ahmed Abu Sharar <absam9945@gmail.com>
2022-08-31 01:05:39 +0300

    fix-signIn relates #18

--- server/controllers/regist/sign-in.js
+++ server/controllers/regist/sign-in.js
@@ -2,40 +2,41 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { signInQuery } = require('../../database/queries');
-const { signInValidate } = require('../../validation/sign-in-validate');
+const signInValidate = require('../../validation/sign-in-validate');
 
 const signIn = (req, res, next) => {
-  const {
-    id,
-    name,
-    email,
-    password,
-    img,
-  } = req.body;
-  const { error, value } = signInValidate().validate(req.body);
+  const { email, password } = req.body;
+  const { error, value } = signInValidate.validate(req.body);
+
   if (error) {
     res.sendStatus(401);
   } else {
-    signInQuery(email)
-      .then((data) => {
-        if (data.rows.length !== 0) {
-          const { hashedPassword } = data.rows[0];
-          return bcrypt.compare(password, hashedPassword);
-        }
-        return res.sendStatus(401).json({ message: 'invalid Input' });
-      })
-      .then((result) => {
-        if (!result) res.sendStatus(200).json({ message: 'invalid Input' });
-        const token = jwt.sign({
+    signInQuery(email).then((data) => {
+      if (data.rows.length !== 0) {
+        const {
+          hashedpassword,
           id,
           name,
-          email,
           img,
-        }, process.env.SECRET);
-        res.cookie('token', token).redirect('/feed');
-      })
-      .catch((err) => next(err));
-    res.json(value);
+        } = data.rows[0];
+
+        bcrypt.compare(password, hashedpassword, (err, result) => {
+          if (err) next();
+          if (!result) res.status(200).json({ message: 'invalid Input' });
+          else {
+            const token = jwt.sign({
+              id,
+              name,
+              email,
+              img,
+            }, process.env.SECRET);
+            res.cookie('token', token, { httpOnly: true }).redirect('/feed');
+          }
+        });
+      } else {
+        res.status(401).json({ message: 'invalid Input' });
+      }
+    });
   }
 };
 
