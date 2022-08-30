c9f52513f02e2f85e842f790eabace0bb1fbe8bb
unknown <saritadh74@gmail.com>
2022-08-31 01:08:55 +0300

    testing code

--- server/routers/regist.js
+++ server/routers/regist.js
@@ -4,5 +4,7 @@ const { signIn, signOut, signUp } = require('../controllers');
 const router = Router();
 
 router.post('/sign-up', signUp);
+router.post('/sign-in', signIn);
+router.get('/sign-out', signOut);
 
 module.exports = router;
