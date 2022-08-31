b8a6be54abb539711ba14483c245a3b5f3191925
tariqabusada <tariqesa101@gmail.com>
2022-08-31 15:45:03 +0200

    feat: rout to delete specific post #35

--- server/routers/posts.js
+++ server/routers/posts.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { addLike, addPost, deletePost } = require('../controllers');
+const userAuthenticator = require('../middlewares/auth');
 
 const router = Router();
 
 router.get('/post/add-like:id', addLike);
 router.post('/post/add-post', addPost);
-router.get('/post/delete-post:id', deletePost);
+router.get('/post/delete-post/:id', userAuthenticator, deletePost);
 
 module.exports = router;
