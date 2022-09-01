f7b1a833c606890f77f5eaa0b7c26e529cd6037e
unknown <saritadh74@gmail.com>
2022-09-01 21:39:02 +0300

    ...

--- server/routers/posts.js
+++ server/routers/posts.js
@@ -4,7 +4,7 @@ const userAuthenticator = require('../middlewares/auth');
 
 const router = Router();
 
-router.get('/post/add-like/:id', addLike);
+router.get('/post/add-like/:id', userAuthenticator, addLike);
 router.post('/post/add-post', userAuthenticator, addPost);
 router.get('/post/delete-post/:id', userAuthenticator, deletePost);
 
