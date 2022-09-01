b2672f5461a0c7a2750e57d3ee68410a586f0f0d
unknown <saritadh74@gmail.com>
2022-09-01 12:22:51 +0300

    project finished

--- server/controllers/user/feed.js
+++ server/controllers/user/feed.js
@@ -1,9 +1,19 @@
+const jwt = require('jsonwebtoken');
+
 const { feedQuery } = require('../../database/queries');
+const { SECRET } = process.env;
+
 
 const feed = (req, res, next) => {
-  feedQuery()
-    .then((data) => res.send(data.rows))
-    .catch((err) => next(err));
+  const { token } = req.cookies;
+
+  jwt.verify(token, SECRET, (err, payload) => {
+    const { id } = payload;
+
+    feedQuery([id])
+      .then((data) => res.send(data.rows))
+      .catch((err) => next(err));
+  });
 };
 
 module.exports = feed;
--- server/database/queries/user/feed-query.js
+++ server/database/queries/user/feed-query.js
@@ -1,6 +1,6 @@
 const connection = require('../../config/connection');
 
-const feedQuery = () =>
-  connection.query('SELECT users.name, users.img, posts.post, posts.likes, posts.id FROM users JOIN posts ON users.id = posts.user_id ORDER BY posts.id;');
+const feedQuery = (id) =>
+  connection.query('SELECT users.name, users.img, posts.post, posts.likes, posts.id FROM users JOIN posts ON users.id = posts.user_id WHERE users.id != ($1) ORDER BY posts.id;', id);
 
 module.exports = feedQuery;
